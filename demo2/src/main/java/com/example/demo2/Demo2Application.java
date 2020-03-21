112d9aa0ddeb58a00c380563f57ad9e10025b2ca
yangfeng <252065840@qq.com>
2020-03-21 14:49:39 +0800

    demo2 text add name

--- demo2/src/main/java/com/example/demo2/Demo2Application.java
+++ demo2/src/main/java/com/example/demo2/Demo2Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Demo2Application {
-
+    private String name;
     public static void main(String[] args) {
         SpringApplication.run(Demo2Application.class, args);
     }
